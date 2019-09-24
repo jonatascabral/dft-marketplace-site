7781f27fedfe10abcaa97e7375dfc6a6ddd299ce
André Posman <andre.posman@gmail.com>
2019-09-24 11:42:30 -0300

    POC finished, adding href's and smooth scroll and minor css fixes.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//suavizando o scroll para as ancoras
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+            inline: 'center'
+        });
+    });
+});
